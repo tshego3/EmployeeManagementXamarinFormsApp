7db931a900fec806b460b8e4bf81853987a85633
Tshegofatso Motswagae <tshegofatsomotswagae@hotmail.com>
2021-02-21 11:04:40 +0200

    Project Update.

---
+++ EmployeeManagementXamarinFormsApp/Services/ApiServices.cs
@@ -0,0 +1,30 @@
+﻿using EmployeeManagementXamarinFormsApp.Models;
+using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EmployeeManagementXamarinFormsApp.Services
+{
+    public class ApiServices
+    {
+        public async Task<bool> RegisterAsync(string email, string password, string confirmPassword)
+        {
+            var client = new HttpClient();
+
+            var model = new RegisterBindingModel
+            {
+                Email = email,
+                Password = password,
+                ConfirmPassword = confirmPassword
+            };
+
+            var json = JsonConvert.SerializeObject(model);
+            HttpContent content = new StringContent(json);
+            var response = await client.PostAsync("http://localhost:800/api/Account/Register", content);
+            return response.IsSuccessStatusCode;
+        }
+    }
+}
--- EmployeeManagementXamarinFormsApp/ViewModels/RegisterViewModel.cs
+++ EmployeeManagementXamarinFormsApp/ViewModels/RegisterViewModel.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using EmployeeManagementXamarinFormsApp.Services;
+using System;
 using System.Collections.Generic;
 using System.Text;
 using System.Windows.Input;
@@ -8,17 +9,30 @@ namespace EmployeeManagementXamarinFormsApp.ViewModels
 {
     public class RegisterViewModel
     {
+        ApiServices _apiServices = new ApiServices();
+
         public string Email { get; set; }
         public string Password { get; set; }
         public string ConfirmPassword { get; set; }
+
+        public string Message { get; set; }
 
         public ICommand RegisterCommand
         {
             get
             {
-                return new Command(() =>
+                return new Command(async() =>
                 {
+                    var IsSuccess = await _apiServices.RegisterAsync(Email, Password, ConfirmPassword);
 
+                    if (IsSuccess)
+                    {
+                        Message = "Registerd Successfully!";
+                    }
+                    else
+                    {
+                        Message = "Please try again later.";
+                    }
                 });
             }
         }
