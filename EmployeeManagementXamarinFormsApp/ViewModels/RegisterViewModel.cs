a47af9e7b3c3bf47c98e9a4baac3bb9e2d1ea36f
Tshegofatso Motswagae <tshegofatsomotswagae@hotmail.com>
2021-02-21 09:16:49 +0200

    Register Model Update.

--- EmployeeManagementXamarinFormsApp/ViewModels/RegisterViewModel.cs
+++ EmployeeManagementXamarinFormsApp/ViewModels/RegisterViewModel.cs
@@ -1,10 +1,26 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Text;
+using System.Windows.Input;
+using Xamarin.Forms;
 
 namespace EmployeeManagementXamarinFormsApp.ViewModels
 {
-    class RegisterViewModel
+    public class RegisterViewModel
     {
+        public string Email { get; set; }
+        public string Password { get; set; }
+        public string ConfirmPassword { get; set; }
+
+        public ICommand RegisterCommand
+        {
+            get
+            {
+                return new Command(() =>
+                {
+
+                });
+            }
+        }
     }
 }
